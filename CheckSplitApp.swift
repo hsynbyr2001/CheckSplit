51c81df857d96acc6aa8a3249b69c8348edbebd6
Hüseyin Bayır <73307531+hsynbyr2001@users.noreply.github.com>
2024-12-29 05:13:47 +0300

    first commit

---
+++ CheckSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitApp.swift
+//  CheckSplit
+//
+//  Created by Hüseyin  Bayır on 29.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
